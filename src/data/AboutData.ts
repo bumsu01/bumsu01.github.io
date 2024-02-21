745f5beca80c9c8404867a44fe11e436aca21961
김범수/디지털통신FE개발 <bumsu01@lguplus.co.kr>
2024-02-21 17:00:37 +0900

    Feature: Profile Homepage Design Change

---
+++ src/data/AboutData.ts
@@ -0,0 +1,15 @@
+interface AboutType {
+  firstName: string;
+  lastName: string;
+  englishName: string;
+  about1: string;
+  about2: string;
+}
+
+export const AboutData: AboutType = {
+  firstName: "Bum-su",
+  lastName: "Kim",
+  englishName: "Glenn",
+  about1: "<A Developer who loves to build cool things />",
+  about2: "<Frontend Developer And Web Designer />",
+};
