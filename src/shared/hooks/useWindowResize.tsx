fc441c33159b22846f0b95510d64975d8bf6bf90
김범수/디지털통신FE개발 <bumsu01@lguplus.co.kr>
2024-03-17 23:04:36 +0900

    Feature: Page init

--- src/shared/hooks/useWindowResize.tsx
+++ src/shared/hooks/useWindowResize.tsx
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
 
 const useWindowResize = () => {
-  type windowSizeType = { width?: number; height?: number };
+  type windowSizeType = {
+    width?: number;
+    height?: number;
+    initSidebar: number;
+  };
   const [windowSize, setWindowSize] = useState<windowSizeType>({
     width: undefined,
     height: undefined,
+    initSidebar: 344,
   });
+
+  const getInitSidebarWidth = () => {
+    if (window.innerWidth < 1024) {
+      return 8;
+    } else if (window.innerWidth < 1512) {
+      return 344;
+    } else {
+      return 500;
+    }
+  };
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -13,6 +28,7 @@ const useWindowResize = () => {
         setWindowSize({
           width: window.innerWidth,
           height: window.innerHeight,
+          initSidebar: getInitSidebarWidth(),
         });
       };
 
