893d8589a72ca5ac2748439cb1581e6d1a573509
김범수/디지털통신FE개발 <bumsu01@lguplus.co.kr>
2024-03-18 23:55:38 +0900

    Chore: Add package

--- src/shared/layouts/app-layout/AppLayout.tsx
+++ src/shared/layouts/app-layout/AppLayout.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import * as S from "./AppLayout.styled";
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
+import { boxStyle, buttonStyle, darkTheme, whiteTheme } from "./AppLayout.css";
+import useStore from "@/store/useStore";
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -13,16 +15,36 @@ export default function AppLayout({
   children,
   pageTitle = "",
 }: AppLayoutProps) {
-  let titleConcat = "Responsive Sidebar Example";
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
 
+  const { bears, increase } = useStore();
+
+  let titleConcat = "Responsive Sidebar Example";
   if (pageTitle) titleConcat = pageTitle + " | " + titleConcat;
+
+  const onClickSwitchDarkModeButton = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
     <S.Centering>
       <Head>
         <title>{pageTitle}</title>
       </Head>
       <S.FixedWidth>
-        <div>Layout Folder(app-layout) - {children}</div>
+        <div className={isDarkTheme ? darkTheme : whiteTheme}>
+          <div className={boxStyle}>box</div>
+          <button className={buttonStyle} onClick={onClickSwitchDarkModeButton}>
+            Button
+          </button>
+          <div>
+            <h1>{bears} around here ...</h1>
+            {/* <button className={buttonStyle} onClick={increase}>
+              one up
+            </button> */}
+          </div>
+          <div>Layout Folder(app-layout) - {children}</div>
+        </div>
       </S.FixedWidth>
     </S.Centering>
   );
