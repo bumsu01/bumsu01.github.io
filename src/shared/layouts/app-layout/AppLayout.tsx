23aa9c79620e6010136f9a65cc7b05eb1ea8b01f
Bumsu Kim <bumsu01@gmail.com>
2024-03-19 21:39:52 +0900

    Feature: Storybook (#35)
    Co-authored-by: 김범수/디지털통신FE개발 <bumsu01@lguplus.co.kr>

--- src/shared/layouts/app-layout/AppLayout.tsx
+++ src/shared/layouts/app-layout/AppLayout.tsx
@@ -4,7 +4,7 @@ import * as S from "./AppLayout.styled";
 import React, { useState } from "react";
 import Head from "next/head";
 import { boxStyle, buttonStyle, darkTheme, whiteTheme } from "./AppLayout.css";
-import useStore from "@/store/useStore";
+import useBearStore from "@/store/useBearStore";
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -17,7 +17,7 @@ export default function AppLayout({
 }: AppLayoutProps) {
   const [isDarkTheme, setIsDarkTheme] = useState<boolean>(false);
 
-  const { bears, increase } = useStore();
+  const { bears, increase, removeAllBears } = useBearStore((state) => state);
 
   let titleConcat = "Responsive Sidebar Example";
   if (pageTitle) titleConcat = pageTitle + " | " + titleConcat;
@@ -32,19 +32,8 @@ export default function AppLayout({
         <title>{pageTitle}</title>
       </Head>
       <S.FixedWidth>
-        <div className={isDarkTheme ? darkTheme : whiteTheme}>
-          <div className={boxStyle}>box</div>
-          <button className={buttonStyle} onClick={onClickSwitchDarkModeButton}>
-            Button
-          </button>
-          <div>
-            <h1>{bears} around here ...</h1>
-            {/* <button className={buttonStyle} onClick={increase}>
-              one up
-            </button> */}
-          </div>
-          <div>Layout Folder(app-layout) - {children}</div>
-        </div>
+        <div>Layout Folder(app-layout)</div>
+        {children}
       </S.FixedWidth>
     </S.Centering>
   );
